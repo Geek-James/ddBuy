4ad4f8ea7c285b8d524b74555b0bf9b6768ad86c
james <639143029@qq.com>
2019-10-10 16:49:37 +0800

    搭建Vuex.

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import State from './state'
+import Mutation from './mutation'
+import Actions from './actions'
+
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-
-    },
-    mutations: {
-
-    },
-    actions: {
-
-    }
+    State,
+    Mutation,
+    Actions
 })
