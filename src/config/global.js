5f7b02998340fff1896e80109b38105c29db4ff9
martinwithyou <406410589@qq.com>
2019-12-19 14:35:59 +0800

    add validate email util function

--- src/config/global.js
+++ src/config/global.js
@@ -335,5 +335,14 @@ export const isIdCard = (str) => {
     return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str)
 }
 
+/**
+ * 
+ * @desc   判断是否为邮箱地址
+ * @param  {String}  str
+ * @return {Boolean} 
+ */
+let isEmail = (str)  => {
+    return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str);
+}
 // 版本信息
 export const _VERSION_ = "1.0.0";
