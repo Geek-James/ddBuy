7beb2dde6e962ab732760452d36fe1be864046fd
martinwithyou <406410589@qq.com>
2019-12-20 14:41:07 +0800

    bugf xied

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import router from './router/router'
 import store from './store/store'
 import FastClick from 'fastclick'
 import VTop from './components/backToTop/ToTop.vue'
+import './icons' // icon
 // 解决移动端点击延迟200ms的问题
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function () {
