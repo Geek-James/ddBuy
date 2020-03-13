40a0f5d5cd6b2b9f02342bc38f6b354bf5b58c0a
james <639143029@qq.com>
2020-03-13 16:03:28 +0800

    Add new version.

--- src/config/global.js
+++ src/config/global.js
@@ -1,23 +1,23 @@
 // 本地化存储
 export const setLocalStore = (name, content) => {
-    if (!name) return;
-    if (typeof content !== 'string') {
-        content = JSON.stringify(content);
-    }
-    window.localStorage.setItem(name, content);
-};
+  if (!name) return
+  if (typeof content !== 'string') {
+    content = JSON.stringify(content)
+  }
+  window.localStorage.setItem(name, content)
+}
 
 // 本地化获取
-export const getLocalStore = (name) => {
-    if (!name) return;
-    return window.localStorage.getItem(name);
+export const getLocalStore = name => {
+  if (!name) return
+  return window.localStorage.getItem(name)
 }
 
 // 本地化删除
-export const removeLocalStore = (name) => {
-    if (!name) return;
-    return window.localStorage.removeItem(name);
+export const removeLocalStore = name => {
+  if (!name) return
+  return window.localStorage.removeItem(name)
 }
 
 // 版本信息
-export const _VERSION_ = "1.3.0";
+export const _VERSION_ = '1.4.0'
