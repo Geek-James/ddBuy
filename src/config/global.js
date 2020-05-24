a83d446dddfcacd369678f625cce31011349c768
james <639143029@qq.com>
2020-05-24 22:12:07 +0800

    🌸Release V1.4.2.

--- src/config/global.js
+++ src/config/global.js
@@ -8,16 +8,16 @@ export const setLocalStore = (name, content) => {
 }
 
 // 本地化获取
-export const getLocalStore = name => {
+export const getLocalStore = (name) => {
   if (!name) return
   return window.localStorage.getItem(name)
 }
 
 // 本地化删除
-export const removeLocalStore = name => {
+export const removeLocalStore = (name) => {
   if (!name) return
   return window.localStorage.removeItem(name)
 }
 
 // 版本信息
-export const _VERSION_ = '1.4.1'
+export const _VERSION_ = '1.4.2'
